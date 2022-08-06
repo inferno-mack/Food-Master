b11458fee167ed1fa185f9b36c9f0fa63c2d79c3
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-06 23:54:25 +0530

    Add files via upload

---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,88 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "The product offers to the consumers an idea and a recipe to cook with two or three fresh ingredients and a Maggi mix. A complete step-by-step recipe is given on the back of the package ",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "aaloo pakoda",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks.",
+
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "They’re all winners but you can’t go wrong with simple, grilled corn on the cob. Bobby amps his up by serving the ears with two butters: one flavored with herbs, the other with garlic and spices. ",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chhola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "As soon as a bite passes the prism of the mouth and touches the taste buds, the softness of the kulcha combined with the spiciness of chole is dispersed into a spectrum of beautiful colour-like flavours. ",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "Pizza is life. The love we have in our hearts for pizza goes deeper than the love we have for any food. Be it farmhouse for lunch, pepperoni for dinner or left-overs for breakfast, a good pizza will never disappoint us. The stuff goes straight to our hearts and we can never imagine getting bored of the most beautiful human creation of all time. ",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Non-vegetarian food is a term commonly used in India for food that has meat, fish or eggs in it. It is more commonly called non-veg food. Not just for food, it can be used for a person too - a person eating meat is a non-vegetarian in Indian English. Egg too is not classified as vegetarian for Indians. Well, they are called eggitarians. ",
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "India is a land of festivals and the best part about this is, if you ask anyone a question on how a particular festival is celebrated in their community or region, the immediate answer would be – by sharing and eating sweets or mithai and lots of them  🙂 The variety of sweets prepared is countless, and each sweet recipe has several regional variations to it. ",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "Rajma chawal or rajma rice is delicious rajma masala (red kidney beans masala) mixed with rice, a comfort food for many who grew up eating this great combination of carbs and protein. While I personally got introduced to the joys of rajma chawal fairly recently, I don’t deny how comforting and filling it can be, especially after a long business trip or a vacation where you don’t eat your usual food for extended periods of time. ",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samosa",
+    category: "evening",
+    price: "10₹",
+    description:
+      " Flaky and crunchy fried samosa are one of the most popular street food snack in North Indian cuisine. They feature a pastry-like crust but are filled with savory potatoes and peas for a hearty, delicious snack.",    
+  },
+  
+];
+
+export default Menu;
